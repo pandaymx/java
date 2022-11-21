ca4d4f80de5a98e4b7cb1e5b91f2a5f3939f6ce3
ymx <1943575780@qq.com>
2022-11-21 17:05:18 +0800

    Java基础

---
+++ project02/src/Demo01.java
@@ -0,0 +1,12 @@
+public class Demo01 {
+    /**
+     * 文档注释
+     * */
+    public static void main(String[] args) {
+        /*多行注释
+        *
+        * */
+        // 单行注释
+        System.out.println("123");
+    }
+}
---
+++ project02/src/Demo04.java
@@ -0,0 +1,13 @@
+// 导包
+import java.util.Scanner;
+
+public class Demo04 {
+
+    public static void main(String[] args) {
+        //创建对象
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个数字：");
+        int i = sc.nextInt();
+        System.out.println(i);
+    }
+}
