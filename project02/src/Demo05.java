59139b19ec91b6be4ac2d99481a61b0381ad7546
ymx <1943575780@qq.com>
2022-11-21 19:26:05 +0800

    基础知识

---
+++ project02/src/Demo05.java
@@ -0,0 +1,13 @@
+public class Demo05 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        // 算数运算符
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        // 有浮点数参与运算，才能得到浮点数
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
---
+++ project02/src/Demo06.java
@@ -0,0 +1,9 @@
+public class Demo06 {
+    public static void main(String[] args) {
+        // 自动类型转换
+        double a = 10;
+        System.out.println(a);
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
