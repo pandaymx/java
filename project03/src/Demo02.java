9c222cec7e4d5c9e8071342ebec315053196b781
ymx <1943575780@qq.com>
2022-11-23 19:46:18 +0800

    数组

---
+++ project03/src/Demo02.java
@@ -0,0 +1,9 @@
+public class Demo02 {
+    public static void main(String[] args) {
+        // 生成二维数组
+        // 第一个为行，第二个为列
+        int[][] arr;
+        arr = new int[3][2];
+        //使用的话通过索引进行实现
+    }
+}
