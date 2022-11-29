f4426915c5aad3794329241cd746566365f129ea
mingxuan yang <1943575780@qq.com>
2022-11-30 00:22:16 +0800

    JDBC

--- project13/src/mysql/Demo04.java
+++ project13/src/mysql/Demo04.java
@@ -1,7 +1,19 @@
 package mysql;
 
-public class Demo04 {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+public class Demo04 {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "123456";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        String sql = "update users set password = 1 where username = 'admin'";
+        int i = statement.executeUpdate(sql);
+        System.out.println(i);
     }
 }
