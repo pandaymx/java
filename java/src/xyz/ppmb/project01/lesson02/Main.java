043d9b7eb6464c091063d374710716dc4142cc52
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-17 18:43:41 +0800

    字面量

---
+++ java/src/xyz/ppmb/project01/lesson02/Main.java
@@ -0,0 +1,40 @@
+package xyz.ppmb.project01.lesson02;
+
+/**
+ * @projectName: code
+ * @package: xyz.ppmb.project01.lesson02
+ * @className: Main
+ * @author: 皮皮萌宝
+ * @description: 字面量
+ * @date: 11/17/23 6:27 PM
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        // 输出10
+        System.out.println(10);
+        // 输出二进制数1011101
+        System.out.println(0b1011101);
+        // 输出八进制数1756431
+        System.out.println(01756431);
+        // 输出十六进制数0x4971bd
+        System.out.println(0x4971bd);
+        // 输出浮点数3.14001
+        System.out.println(3.14001);
+        // 输出科学计数法表示的浮点数2.5e6
+        System.out.println(2.5e6);
+        // 输出布尔值true
+        System.out.println(true);
+        // 输出布尔值false
+        System.out.println(false);
+        // 输出字符'中'
+        System.out.println('中');
+        // 输出字符串"Hello World"
+        System.out.println("Hello World");
+        // 不能直接输出null
+        // System.out.println(null);
+        String a = null;
+        // 输出null
+        System.out.println(a);
+    }
+}
