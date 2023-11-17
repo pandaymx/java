adb3fd3c4f427be9e2850f9a052242f9c51e2c64
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-17 21:53:51 +0800

    算数运算符

---
+++ java/src/xyz/ppmb/project01/lesson04/Main.java
@@ -0,0 +1,23 @@
+package xyz.ppmb.project01.lesson04;
+
+/**
+ * @projectName: java
+ * @package: xyz.ppmb.project01.lesson04
+ * @className: Main
+ * @author: 皮皮萌宝
+ * @description: 算数运算符
+ * @date: 2023/11/17 21:52
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a / b = " + (a / 20.));
+        System.out.println("a % b = " + (a % b));
+    }
+}
