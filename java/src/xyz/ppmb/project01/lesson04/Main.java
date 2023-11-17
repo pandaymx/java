12f0376beca6a55ab40d8a09935b9c558b4eedf3
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-17 23:04:57 +0800

    算数运算符

--- java/src/xyz/ppmb/project01/lesson04/Main.java
+++ java/src/xyz/ppmb/project01/lesson04/Main.java
@@ -10,14 +10,15 @@ package xyz.ppmb.project01.lesson04;
  * @version: 1.0
  */
 public class Main {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("a / b = " + (a / b));
-        System.out.println("a / b = " + (a / 20.));
-        System.out.println("a % b = " + (a % b));
+        public static void main(String[] args) {
+        int a = 10; // 定义整型变量a并赋值为10
+        int b = 20; // 定义整型变量b并赋值为20
+        System.out.println("a + b = " + (a + b)); // 打印输出a与b的和
+        System.out.println("a - b = " + (a - b)); // 打印输出a与b的差
+        System.out.println("a * b = " + (a * b)); // 打印输出a与b的积
+        System.out.println("a / b = " + (a / b)); // 打印输出a除以b的商（整除）
+        System.out.println("a / b = " + (a / 20.0)); // 打印输出a除以20的商（浮点除）
+        System.out.println("a % b = " + (a % b)); // 打印输出a除以b的余数
     }
+
 }
