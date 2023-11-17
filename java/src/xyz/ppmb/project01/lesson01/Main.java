b4ffc6e569ef8069d8bdffc34ea94101b5be8b53
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-17 18:26:56 +0800

    Hello World

---
+++ java/src/xyz/ppmb/project01/lesson01/Main.java
@@ -0,0 +1,16 @@
+package xyz.ppmb.project01.lesson01;
+
+/**
+ * @projectName: code
+ * @package: xyz.ppmb.project01.lesson01
+ * @className: Main
+ * @author: 皮皮萌宝
+ * @description: Hello World
+ * @date: 11/17/23 6:26 PM
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
