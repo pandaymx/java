80ba4fe2b11eefaf71ec3454f8c856bb732fe940
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-17 21:36:13 +0800

    变量和数据类型

---
+++ java/src/xyz/ppmb/project01/lesson03/Main.java
@@ -0,0 +1,32 @@
+package xyz.ppmb.project01.lesson03;
+
+/**
+ * @projectName: code
+ * @package: xyz.ppmb.project01.lesson03
+ * @className: Main
+ * @author: 皮皮萌宝
+ * @description: 变量和数据类型
+ * @date: 11/17/23 6:43 PM
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        // int a表示定义一个变量，a=10表示给变量赋值 ，两者可以结合
+        int a = 10;
+        byte b = 30;
+        short c = 40;
+        long d = 50L;
+        float e = 50.0f;
+        double f = 32.5;
+        char g = 'a';
+        boolean h = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
