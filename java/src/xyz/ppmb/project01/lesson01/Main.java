147d8259c6619dd59d4116a3cd176f5539a14597
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-17 18:27:30 +0800

    加上注释

--- java/src/xyz/ppmb/project01/lesson01/Main.java
+++ java/src/xyz/ppmb/project01/lesson01/Main.java
@@ -5,12 +5,16 @@ package xyz.ppmb.project01.lesson01;
  * @package: xyz.ppmb.project01.lesson01
  * @className: Main
  * @author: 皮皮萌宝
- * @description: Hello World
+ * @description: Hello World和注释
  * @date: 11/17/23 6:26 PM
  * @version: 1.0
  */
 public class Main {
+    // 单行注释
     public static void main(String[] args) {
+        /*
+        * 多行注释，最上面为文档注释
+        * */
         System.out.println("Hello World");
     }
 }
